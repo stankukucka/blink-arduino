40d7570ae0b0c5ba1340cc3944529908dbb656a3
Stan Kukucka <47703034+stankukucka@users.noreply.github.com>
2020-11-28 21:31:25 +0100

    Update blink-arduino.py

--- blink-arduino.py
+++ blink-arduino.py
@@ -1,23 +1,23 @@
-from pyfirmata2 import Arduino
-import time
+from pyfirmata2 import Arduino #import library from pyfirmata2 to detect Arduino
+import time #time library to be able setup lenght of led lighting
 
-board = Arduino(Arduino.AUTODETECT)
+board = Arduino(Arduino.AUTODETECT) #detect Arduino with Autodetect
 
-print("...Arduino detected")
-print("...Blink test started")
+print("...Arduino detected") #print statement #1
+print("...Blink test started") #print statement #2
 
-while True:
+while True: #while this statement is true execute script hereunder
     board.digital[13].write(1)
-    time.sleep(2)
+    time.sleep(2) #light up led number 13 for 2 seconds
     board.digital[13].write(0)
-    time.sleep(1)
+    time.sleep(1) #light off led number 13 for 1 seconds
     board.digital[13].write(1)
-    time.sleep(2)
+    time.sleep(2) #light up led number 13 for 2 seconds
     board.digital[13].write(0)
-    time.sleep(1)
+    time.sleep(1) #light off led number 13 for 1 seconds
     board.digital[13].write(1)
-    time.sleep(2)
+    time.sleep(2) #light up led number 13 for 2 seconds
     board.digital[13].write(0)
-    time.sleep(1)
-    print("...Blink test successfull!")
-    exit()
+    time.sleep(1) #light off led number 13 for 1 seconds
+    print("...Blink test successfull!") #print statement #3
+    exit() #exit script
